79d0a1841427e3d0467b50b4041d4a1181ed3e87
qqwwee2006@gmail.com <qqwwee2006@gmail.com>
2018-12-31 13:55:23 +0800

    add example 1-9

---
+++ example/1-9/main.js
@@ -0,0 +1,22 @@
+﻿window.onload = function() {
+    var photos;
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            photos = JSON.parse(this.responseText);
+        }
+    }
+    req.open('GET', 'photos.json', false);
+    req.send();
+
+    var i = 0;
+    function run() {
+        if (i >= photos.length) {
+            i = 0;
+        }
+        document.getElementById("image").setAttribute('src', photos[i]);
+        i++;
+    }
+    setInterval(run, 3000);
+    run();
+}
