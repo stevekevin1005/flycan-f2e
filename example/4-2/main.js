883414b86382e9ad8e79614b2f7971318eda0a71
qqwwee2006@gmail.com <qqwwee2006@gmail.com>
2019-01-29 02:39:34 +0800

    add example 4-2

---
+++ example/4-2/main.js
@@ -0,0 +1,9 @@
+﻿(function() {
+    var text = "PC";
+    if (/android/i.test(navigator.userAgent)) {
+        text = "Android";
+    } else if (/iphone/i.test(navigator.userAgent)) {
+        text = "iPhone";
+    }
+    document.getElementsByTagName("body")[0].innerText = `${text} 版網頁`;
+})();
