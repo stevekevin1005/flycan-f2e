ed9c0af85ffd3e77b4e14438af37c1af3a4bc179
qqwwee2006@gmail.com <qqwwee2006@gmail.com>
2019-01-30 00:22:09 +0800

    add example 4-4

---
+++ example/4-4/main.js
@@ -0,0 +1,5 @@
+﻿(function() {
+    document.getElementById("hi").addEventListener("click", function(){
+       document.getElementById("show_name").innerHTML = document.getElementById("name").value;
+    });
+})();
