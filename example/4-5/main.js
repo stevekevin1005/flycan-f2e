b82b56830cb922f9899ddda963c81a93ede091e4
qqwwee2006@gmail.com <qqwwee2006@gmail.com>
2019-01-30 01:43:19 +0800

    add example 4-5

---
+++ example/4-5/main.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    var cookie = document.cookie;
+    if (cookie.includes("name")) {
+        document.getElementById('message').innerText = "Hello " + getCookie("name");
+    }
+    else {
+        document.getElementById('message').innerText = 'please login';
+    }
+
+    document.getElementById("login").addEventListener("click", function(){
+        document.cookie = " name=" + document.getElementById("name").value;
+    });
+
+    function getCookie(name) {
+        var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+        if (match) return match[2];
+    }
+})();
