9675d54f6c89a744fca84195f159d4f045811b6f
qqwwee2006@gmail.com <qqwwee2006@gmail.com>
2019-01-30 21:14:30 +0800

    add example 4-6

---
+++ example/4-6/main.js
@@ -0,0 +1,19 @@
+﻿(function() {
+    var cookie = document.cookie;
+    if (cookie.includes("check")) {
+        alert("notice");
+    }
+
+    document.getElementById("cancel").addEventListener("click", function(){
+        document.cookie = 'check=true; expires=Fri, 31 Dec 1970 23:59:59 GMT';
+    });
+
+    document.getElementById("check").addEventListener("click", function () {
+        document.cookie = 'check=true; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+    });
+
+    function getCookie(name) {
+        var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+        if (match) return match[2];
+    }
+})();
