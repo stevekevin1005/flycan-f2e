a3c1d3993c7993cb8dc2ad26dce05c4c4aee2b82
qqwwee2006@gmail.com <qqwwee2006@gmail.com>
2019-02-08 19:15:45 +0800

    add example 5-1

---
+++ example/5-1/main.js
@@ -0,0 +1,13 @@
+﻿(function() {
+    if (Cookies.get('name') != undefined) {
+        document.getElementById('message').innerText = "Hello " + Cookies.get('name');
+    }
+    else {
+        document.getElementById('message').innerText = 'please login';
+    }
+
+    document.getElementById("login").addEventListener("click", function(){
+        Cookies.set("name", document.getElementById("name").value);
+    });
+
+})();
