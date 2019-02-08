9d7281f5a80be8107ea5f945c9430f35f0b3a190
qqwwee2006@gmail.com <qqwwee2006@gmail.com>
2019-02-09 00:05:19 +0800

    add example 5-2

---
+++ example/5-2/main.js
@@ -0,0 +1,22 @@
+﻿(function() {
+    function checkLogin() {
+        if (Cookies.get('name') != undefined) {
+            let html = `Hello ${Cookies.get('name')} <button id="logout">Log out</button>`;
+            document.getElementById('content').innerHTML = html;
+            document.getElementById('logout').addEventListener('click', function () {
+                Cookies.remove('name');
+                checkLogin();
+            });
+        }
+        else {
+            let html = `<h3>please login</h3>
+                        <input type="text" id="name"><button id="login">login</button>`;
+            document.getElementById('content').innerHTML = html;
+            document.getElementById("login").addEventListener("click", function () {
+                Cookies.set("name", document.getElementById("name").value);
+                checkLogin();
+            });
+        }
+    }
+    checkLogin();
+})();
