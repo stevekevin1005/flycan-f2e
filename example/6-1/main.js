a407fc132c473bdfc34b3e105372bc0f47e1356b
qqwwee2006@gmail.com <qqwwee2006@gmail.com>
2019-02-10 15:31:57 +0800

    add example 6-1

---
+++ example/6-1/main.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    if (new Date().getHours < 12 ) {
+        document.getElementById("content").innerText = "上午";
+    }
+    else {
+        document.getElementById("content").innerText = "下午";
+    }
+
+    new Date().getHours < 12 ? document.getElementById('content').innerText = '上午' : document.getElementById("content").innerText = "下午";
+})();
